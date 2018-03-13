a89ad855b2bb7396ffaba66710f574f4385d0898
NoeLevi <noelevytete@gmail.com>
2018-03-13 13:34:25 +0200

    actual 13

--- src/main/resources/static/js/scripty.js
+++ src/main/resources/static/js/scripty.js
@@ -1,3 +1,16 @@
+/** 
+ * scroll
+ * */
+$('nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 1500);
+});
+
 var arrayIdsElementsPage = new Array;
 
 
