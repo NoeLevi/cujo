907fe9d7ec0eb5339053aff0d27acb418c464628
NoeLevi <noelevytete@gmail.com>
2018-03-14 09:03:06 +0200

    ACTUAL 21

--- src/main/java/mineira/cujo/SenderMailService.java
+++ src/main/java/mineira/cujo/SenderMailService.java
@@ -1,17 +1,22 @@
 package mineira.cujo;
 
+import javax.faces.bean.ManagedBean;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-@Component
+@Controller
+@Scope(value = "session")
+@ManagedBean(name = "emailBean")
 public class SenderMailService {
 
     @Autowired
     private JavaMailSender mailSender;
 
-    private  void enviar() {
+    public  void enviar() {
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo("noelevytete@gmail.com");
         email.setSubject("Teste envio de e-mail");
